e65cb526afee3a44f8f2a44d7813b03f255686da
Michael Shambel <Michaelshambel10@gmail.com>
2023-11-27 13:43:38 -0800

    linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *check_cycle - check if singly linked list has cycle
+ *@list: pointer list
+ *
+ * Return: 0 or 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
