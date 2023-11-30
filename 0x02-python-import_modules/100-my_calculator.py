c039fbba6d37d9c95db8abdedbe25324aa3a3746
Michael Shambel <Michaelshambel10@gmail.com>
2023-11-30 09:33:40 -0800

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,3 +7,22 @@ if __name__ == "__main__":
     if num_arg != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+    a = sys.argv[1]
+    sign = sys.argv[2]
+    b = sys.argv[3]
+    if sign not in ['+', '-', '*', '/']:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(a)
+    b = int(b)
+
+    if sign == '+':
+        answer = add(a, b)
+    elif sign == '-':
+        answer = sub(a, b)
+    elif sign == '*':
+        answer = mul(a, b)
+    else:
+        answer = div(a, b)
+    print("{} {} {} = {}".format(a, sign, b, answer))
