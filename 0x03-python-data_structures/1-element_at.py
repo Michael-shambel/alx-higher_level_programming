ad6037377fd8b06e6f20d8406eb0217dc4ccef1b
Michael Shambel <Michaelshambel10@gmail.com>
2023-12-02 00:45:04 -0800

    element inthe list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if(idx < 0):
-        return none
+        return 'none'
     elif(idx > len(my_list) - 1):
-        return none
+        return 'none'
     else:
         return my_list[idx]
