457a518a440b5fb1b8a69860e1fb99cdf1f06523
Michael Shambel <Michaelshambel10@gmail.com>
2023-12-06 02:51:57 -0800

    error to return

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if (len(my_list) == 0):
+    if my_list is None:
         return my_list
-    my_list.reverse()
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+    else:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
