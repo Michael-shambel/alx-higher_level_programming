2e06ab5d287c0ca4da2f053b54d188d037ea1e61
Michael-shambel <Michaelshambel10@gmail.com>
2024-01-01 02:36:20 -0800

    error found

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     length_str = len(sentence)
     if length_str == 0:
-        return 'None'
+        return None
     else:
         return length_str, sentence[0]
