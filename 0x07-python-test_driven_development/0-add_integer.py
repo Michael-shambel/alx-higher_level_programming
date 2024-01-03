88ad087e309de668b1a3efacafadd42c49d2c63c
Michael-shambel <Michaelshambel10@gmail.com>
2024-01-03 13:42:12 -0800

    task 0 commit 2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,18 +10,10 @@ def add_integer(a, b=98):
     :rtype: int
     """
 
-    if a is None:
-        raise TypeError("a must be an integer")
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or float")
-
-    if isinstance(b, str):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer or float")
 
-    a = int(a)
-    b = int(b)
-
-    return a + b
+    return (int(a) + int(b))
