839b7dda8e26e8e70fb5f219f41725c66a70aca8
Michael-shambel <Michaelshambel10@gmail.com>
2024-01-04 04:30:04 -0800

    task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""calculate area and perimeter of rectangle"""
+
+
+class Rectangle:
+    """clalculate the perimeter and area of the retangle"""
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        perim = 2 * (self.__width + self.__height)
+        if self.__width == 0 or self.__height == 0:
+            perim = 0
+        return perim
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        str_rec = ""
+        for _ in range(self.__height):
+            str_rec += '#' * self.__width + '\n'
+        return str_rec
+
+    def __repr__(self):
+        return "<{}.{} object at {}>"
+    .format(type(self).__module__, type(self).__name__, hex(id(self)))
