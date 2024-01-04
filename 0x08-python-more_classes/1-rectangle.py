6bf687216e748ff7ad776511b36f54b6d1da7b38
Michael-shambel <Michaelshambel10@gmail.com>
2024-01-04 03:08:37 -0800

    error in missplacing value

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """calculate the rectangle"""
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
