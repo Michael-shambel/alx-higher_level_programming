69d194db3a5328db5b35a218f9f2671ce8e98171
Michael-shambel <Michaelshambel10@gmail.com>
2024-01-06 12:09:57 -0800

    task 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
+"""define the addition of integer"""
+
+
 def add_integer(a, b=98):
     """
-    addition of two integers with the value of interger or float
+    Addition of two integers with the value of integer or float.
+
     :param a: First parameter
     :type a: int or float
-    :param b: second parameter
+    :param b: Second parameter (default is 98)
     :type b: int or float
-    :return: the sum of two int or float
+    :return: The sum of two int or float
     :rtype: int
     """
 
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
 
 
