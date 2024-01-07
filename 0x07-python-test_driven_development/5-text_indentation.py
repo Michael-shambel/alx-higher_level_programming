376d9c0d155abd959e0ba621873c6be36fb952ba
Michael-shambel <Michaelshambel10@gmail.com>
2024-01-07 15:50:36 -0800

    task 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""this function going to check every text if any specified function showed it will print the line"""
+
+
+def text_indentation(text):
+    
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    sign = ['.', '?', ':']
+    lines = []
+    l_count = ''
+
+    for char in text:
+        l_count += char
+        if char in sign:
+            lines.append(l_count.strip())
+            l_count = ''
+
+    if l_count:
+        lines.append(l_count.strip())
+
+    for l_count in lines:
+        print(l_count)
+        print()
