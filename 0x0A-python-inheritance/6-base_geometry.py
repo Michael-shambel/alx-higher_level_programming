007500067b225b3b8daba3b8ece8d5f37a2b71c0
Michael-shambel <Michaelshambel10@gmail.com>
2024-01-08 14:23:18 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" define base geometry """
+
+
+class BaseGeometry:
+    """ class of basegeometry """
+    def area(self):
+        raise Exception("area() is not implemented")
