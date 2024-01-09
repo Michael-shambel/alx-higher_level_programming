77e25ccc9d706d8aa7abc50a7742789caaadcbf2
Michael-shambel <Michaelshambel10@gmail.com>
2024-01-09 10:44:17 -0800

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 import sys
+"""this function accept argument from system and add to the python list and
+save them in txt"""
+
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -10,6 +13,5 @@ if __name__ == "__main__":
         data = load_from_json_file('add_item.json')
     except FileNotFoundError:
         data = []
-
     data.extend(sys.argv[1:])
     save_to_json_file(data, 'add_item.json')
