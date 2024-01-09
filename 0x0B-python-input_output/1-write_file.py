a8df79b17b24b9af0ee4a2ce3190689a521fe1bb
Michael-shambel <Michaelshambel10@gmail.com>
2024-01-09 01:03:35 -0800

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""unction that writes a string to a text file and returns the number of characters written"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file and returns the number of characters
+    args:
+    filename: name of a file
+    text: text that printed and counted
+    return:
+    a text file and number of characted in the file
+    """
+    with open(filename, 'w', encoding='UTF8') as f:
+        number_of_char = f.write(text)
+    return number_of_char
