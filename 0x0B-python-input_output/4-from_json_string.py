ea819124dc0b0c0a3653fcbf1652a0b8f6b191a9
Michael-shambel <Michaelshambel10@gmail.com>
2024-01-09 09:40:09 -0800

    task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -9,4 +9,9 @@ import json
 
 
 def from_json_string(my_str):
+    """ this function returns an object repreented by JSON
+    args:
+    my_str: string
+    return:
+    object of json"""
     return json.loads(my_str)
