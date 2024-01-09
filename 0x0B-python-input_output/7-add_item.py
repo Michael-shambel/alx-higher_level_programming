cde298baae28f08c4333fb1a43606efeabd94fea
Michael-shambel <Michaelshambel10@gmail.com>
2024-01-09 10:46:00 -0800

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -13,5 +13,6 @@ if __name__ == "__main__":
         data = load_from_json_file('add_item.json')
     except FileNotFoundError:
         data = []
+    
     data.extend(sys.argv[1:])
     save_to_json_file(data, 'add_item.json')
