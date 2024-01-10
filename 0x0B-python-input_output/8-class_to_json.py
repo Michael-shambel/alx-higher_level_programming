6b20957c35d04678856155b8a8e923e40fec124d
Michael-shambel <Michaelshambel10@gmail.com>
2024-01-09 23:07:44 -0800

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+""" function that return dictionary with simple data structure"""
 
 def class_to_json(obj):
+    """return a dictionary og an object"""
     return obj.__dict__
