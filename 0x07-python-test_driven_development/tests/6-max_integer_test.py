984cb5a70c3895e3344bb0a37e667baf8430ea6e
Michael-shambel <Michaelshambel10@gmail.com>
2024-01-10 15:25:20 -0800

    task 6

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""unitest for matrix"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    
+    def test_regular_list(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+    
+    def test_unordered_list(self):
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+    
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -5, -3, -7]), -1)
+
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([-1, 0, 5, -3, 7]), 7)
+
+    def test_duplicate_max(self):
+        self.assertEqual(max_integer([1, 2, 3, 4, 4]), 4)
+
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
+if __name__ == '__main__':
+    unittest.main()
