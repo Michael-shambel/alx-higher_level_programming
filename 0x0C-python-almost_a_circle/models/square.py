04cb9f292736eba21abd634c475107c498c7deb2
Michael-shambel <Michaelshambel10@gmail.com>
2024-01-14 14:06:11 -0800

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,7 +6,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """this class is about a square class"""
 
-    def __init__ (self, size, x=0, y=0, id=None):
+    def __init__(self, size, x=0, y=0, id=None):
         """this is the constranctor of a square inherited from
         Rectangle class and it is going to take the attributes
         of rectangle"""
@@ -15,3 +15,14 @@ class Square(Rectangle):
     def __str__(self):
         """this print the attributes"""
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
+
+    @property
+    def size(self):
+        """this is getter for attribute"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """this is setter for attribute"""
+        self.width = value
+        self.height = value
