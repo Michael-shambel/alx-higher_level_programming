fa761b80de26f4b96f65f0f55b3f44038a762e11
Michael-shambel <Michaelshambel10@gmail.com>
2024-02-05 14:50:25 -0800

    task 2 again and agian

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -30,7 +30,6 @@ def matrix_divided(matrix, div):
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
