aa8488603a1d0206031c7c866aa59deb2cd54b6b
Michael-shambel <Michaelshambel10@gmail.com>
2024-02-15 12:50:58 -0800

    error1100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,5 @@
-USE hbtn_0c_0;
+USE `hbtn_0c_0`
 
 -- Convert the table's charset and collation
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the charset and collation of the 'name' field
-ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
