5558412287e19b782b7f6dd4b38d6044fb45a1d5
Michael-shambel <Michaelshambel10@gmail.com>
2024-03-12 11:08:09 -0700

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const linestring = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+for (let i = 0; i < linestring.length; i++) {
+  console.log(linestring[i]);
+}
