b34e4acc3c1f3db4f7a53494744ea2d02d980024
michael-shambel <Michaelshambel10@gmail.com>
2024-03-15 06:06:52 -0700

    task 3

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_user,
+            passwd=mysql_password,
+            db=database_name
+            )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    values = cursor.fetchall()
+    for value in values:
+        print(value)
+    cursor.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_user,
+            passwd=mysql_password,
+            db=database_name
+            )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = %s", (state_name,))
+    values = cursor.fetchall()
+    for value in values:
+        print(value)
+    cursor.close()
+    db.close()
