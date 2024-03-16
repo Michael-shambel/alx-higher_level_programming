6771984e38a56d69a18332ab7f4f301a97bafa31
michael-shambel <Michaelshambel10@gmail.com>
2024-03-16 09:30:49 -0700

    task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
             db=database_name
             )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE %s", (state_name,))
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s", (state_name, ))
     values= cursor.fetchall()
     for value in values:
         print(value)
