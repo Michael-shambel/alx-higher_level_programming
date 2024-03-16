f6b15f15d47652ce4381400aef8cd38e407b29cb
michael-shambel <Michaelshambel10@gmail.com>
2024-03-16 09:52:17 -0700

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     mysql_user = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
-    
+
     db = MySQLdb.connect(
             host="localhost",
             port=3306,
