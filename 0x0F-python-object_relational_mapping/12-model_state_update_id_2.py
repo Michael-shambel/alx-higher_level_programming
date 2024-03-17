58863e073b35a4924f81ea28a310a4f111246871
michael-shambel <Michaelshambel10@gmail.com>
2024-03-17 05:32:28 -0700

    task 12e

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state.name = "New Mexico"
     state = session.query(State).filter_by(id=2).first()
+    state.name = 'New Mexico'
     session.commit()
     session.close()
