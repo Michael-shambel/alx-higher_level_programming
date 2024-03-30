467139330c0269fcc3845579cf6f137c9d217fb1
michael-shambel <Michaelshambel10@gmail.com>
2024-03-30 00:29:59 -0700

    task 1 upd

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the body of the response if the status code is 200.
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s -L "$1"
+curl -s -o -L /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s -L "$1"
