70df05cac0b2da792ba9269565971caafcf8fe2d
michael-shambel <Michaelshambel10@gmail.com>
2024-03-30 06:32:12 -0700

    task 4 ypy

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ Python script that fetches url"""
 import urllib.request
-url = 'https://alx-intranet.hbtn.io/status'
-rep = urllib.request.get(url)
-print("Body response:")
-print("\t- type: {}".format(type(rep)))
-print("\t- content: {}".format(rep))
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    rep = urllib.request.urlopen(url)
+    body = rep.read().decode('utf8')
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
