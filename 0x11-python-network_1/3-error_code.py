c43ef4be589253adb0f109badab3c6aa30e7cd03
michael-shambel <Michaelshambel10@gmail.com>
2024-03-30 06:06:39 -0700

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL, sends a request to the URL
+and displays the body of the response
+You have to manage urllib.error.HTTPError exceptions and print:
+Error code: followed by the HTTP status code """
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            body = response.read()
+            print(body)
+    except urllib.error.HTTPError as e:
+        er_code = e.code
+        print(er_code)
