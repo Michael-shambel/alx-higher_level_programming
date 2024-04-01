9853dcd0206b9e1d50fd3cdaef13750a5123848e
michael-shambel <Michaelshambel10@gmail.com>
2024-04-01 02:19:02 -0700

    task 8 upd

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,17 +5,19 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
     url = 'http://0.0.0.0:5000/search_user'
     value = {"q": letter}
 
     req = requests.post(url, data=value)
     try:
         resp = req.json()
-        if resp == {}:
+        if isinstance(resp, dict) and 'id' in resp and 'name' in resp:
+            print("[{}] {}".format(resp['id'], resp['name']))
+        elif resp == {}:
             print("No result")
         else:
-            print("[{}] {}".format(resp.get("id"), resp.get("name")))
+            print("Not a valid JSON")
     except ValueError:
         print("Not a valid JSON")
 
