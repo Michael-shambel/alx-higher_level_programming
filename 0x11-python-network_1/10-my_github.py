fc8cd88d8e9ad067b68e20b552f7060903d8697b
michael-shambel <Michaelshambel10@gmail.com>
2024-04-01 09:07:32 -0700

    task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,7 +7,7 @@ import requests
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    url = 'https://api.github.com'
+    url = 'https://api.github.com/user'
 
     resp = requests.get(url, auth=(username, password))
     if resp:
