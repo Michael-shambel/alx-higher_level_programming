2211d63c567646204a57c726536dbaf94d2ee769
michael-shambel <Michaelshambel10@gmail.com>
2024-04-01 08:26:34 -0700

    task 9 ee

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,6 +7,7 @@ import requests
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
+    url = 'https://api.github.com'
 
     resp = requests.get(url, auth=(username, password))
     data = resp.json()
