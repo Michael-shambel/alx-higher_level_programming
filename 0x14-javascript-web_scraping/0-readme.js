e1ec535dd9054c31638acf1c48f66520bd0974b9
michael-shambel <Michaelshambel10@gmail.com>
2024-04-23 11:06:19 -0700

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,9 +5,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err);
-		process.exit(1);
+  if (err) {
+    console.error('Error reading the file:', err);
+		
 	}
 	console.log(data);
 });
